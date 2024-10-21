b81c1ce91fe017f9dbbb0c94cb285571314f7c8e
victortomaz08 <141277700+victortomaz08@users.noreply.github.com>
2024-10-21 11:19:59 -0300

    ok

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a resposta da equação (8x-15=3x)',
+    '3'
+)
+
+criaCartao(
+    'História',
+    'Quando foi a descoberta do Brasil?',
+    '22 de abril de 1500'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
